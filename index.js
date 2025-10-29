6e082342509256f25b58ac27459a5c7b90eb6105
Avinash Kumar <avinashkumar90202@gmail.com>
2025-10-29 05:31:04 +0530

    Initial commit - URL shortener using Express

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+const http = require('http');
+
+const server = http.createServer(app);
+
+
+
+const urlStore = {};
+
+app.get('/geturl',(req,res)=>{
+    const url = req.query.url;
+    const code = Math.floor(1000 + Math.random() * 9000).toString();
+    urlStore[code] = url;
+    res.send(`Your shortUrl: http://localhost:3000/${code}`);
+    
+
+});
+
+app.get('/:code',(req,res)=>{
+    if(urlStore.hasOwnProperty(req.params.code)){
+        res.redirect(urlStore[req.params.code]);
+    }
+    else{
+        res.send('Invalid URL');
+    }
+});
+
+app.get('/showall', (req, res) => {
+    let output = '';
+    for (let key in urlStore) {
+      output += `${key} → ${urlStore[key]}<br>`;
+    }
+    res.send(output || 'No URLs stored yet');
+  });
+  
+
+server.listen(3000, ()=>{
+    console.log('server is running on port 3000');
+})
