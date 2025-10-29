8e4c0549fc9947539457a14159c565ee2b7e483a
Avinash Kumar <avinashkumar90202@gmail.com>
2025-10-29 06:08:51 +0530

    added url checks and format for url

--- index.js
+++ index.js
@@ -10,7 +10,28 @@ const server = http.createServer(app);
 const urlStore = {};
 
 app.get('/geturl',(req,res)=>{
-    const url = req.query.url;
+    let url = req.query.url;
+    if (!url) {
+        return res.status(400).send('Missing URL parameter');
+      }
+
+
+      if (!/^[a-zA-Z][a-zA-Z\d+\-.]*:\/\//.test(url)) {
+        if (url.startsWith('ftp.')) {
+          return res.status(400).send('Please include the correct protocol (e.g., ftp://ftp.server.com)');
+        }
+        // default to http if no protocol
+        url = `http://${url}`;
+      }
+
+    
+    try {
+        new URL(url);
+      } catch {
+        return res.status(400).send('Invalid URL format');
+      }
+
+    
     const code = Math.floor(1000 + Math.random() * 9000).toString();
     urlStore[code] = url;
     res.send(`Your shortUrl: http://localhost:3000/${code}`);
